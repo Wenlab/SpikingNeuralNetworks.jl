9883f305e58756a9809d624515bd4b35fddad13c
Irismoon <1121490236@qq.com>
2020-11-06 13:25:09 +0800

    eliminate SNN

--- examples/pinning_net.jl
+++ examples/pinning_net.jl
@@ -1,10 +1,10 @@
-using Plots, SNN; plotly(); #reload("SNN")
+using Plots, SpikingNeuralNetworks; plotly(); #reload("SNN")
 
-S = SNN.Rate(;N = 200)
-SS = SNN.PINningSynapse(S, S; σ = 1.5, p = 1.0)
+S = SpikingNeuralNetworks.Rate(;N = 200)
+SS = SpikingNeuralNetworks.PINningSynapse(S, S; σ = 1.5, p = 1.0)
 P, C = [S], [SS]
 
-SNN.monitor(SS, [(:g, [1])])
+SpikingNeuralNetworks.monitor(SS, [(:g, [1])])
 
 A = 1.3 / 1.5; fr = 1 / 60ms
 f(t) = (A /1.0) * sin(1π * fr * t) +  (A / 2.0) * sin(2π * fr * t) +
@@ -13,7 +13,7 @@ f(t) = (A /1.0) * sin(1π * fr * t) +  (A / 2.0) * sin(2π * fr * t) +
 ts = 0:0.1ms:1440ms
 for (i, t) in enumerate(ts)
     SS.f .= [f(t); SS.g[2:end]]
-    SNN.train!(P, C, 0.1ms, t)
+    SpikingNeuralNetworks.train!(P, C, 0.1ms, t)
 end
 
-SNN.vecplot(SS, :g); plot!(f.(ts)) |> gui
+SpikingNeuralNetworks.vecplot(SS, :g); plot!(f.(ts)) |> gui
